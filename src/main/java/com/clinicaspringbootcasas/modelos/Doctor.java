fe0b59eedb134066639988c7395710ae366129d1
DanielKsas <danielmorenocasas1234@gmail.com>
2025-10-24 11:15:03 -0500

    Updated

--- src/main/java/com/clinicaspringbootcasas/modelos/Doctor.java
+++ src/main/java/com/clinicaspringbootcasas/modelos/Doctor.java
@@ -16,7 +16,7 @@ public class Doctor {
     private String specialty;
     private String phone;
     private String email;
-    private String AttentionSchedule;
+    private String attentionSchedule;
     private String office;
     private String experience;
 
@@ -26,7 +26,7 @@ public class Doctor {
     public Doctor() {
     }
 
-    public Doctor(Integer id, String fullName, String documentType, String documentNumber, String specialty, String phone, String email, String attentionSchedule, String office, String experience) {
+    public Doctor(Integer id, String fullName, String documentType, String documentNumber, String specialty, String phone, String email, String attentionSchedule, String office, String experience, ProfessionalCard professionalCard) {
         this.id = id;
         this.fullName = fullName;
         this.documentType = documentType;
@@ -34,88 +34,97 @@ public class Doctor {
         this.specialty = specialty;
         this.phone = phone;
         this.email = email;
-        AttentionSchedule = attentionSchedule;
+        this.attentionSchedule = attentionSchedule;
         this.office = office;
         this.experience = experience;
+        this.professionalCard = professionalCard;
     }
 
     public Integer getId() {
         return id;
     }
 
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getDocumentType() {
-        return documentType;
-    }
-
-    public String getDocumentNumber() {
-        return documentNumber;
-    }
-
-    public String getSpecialty() {
-        return specialty;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getAttentionSchedule() {
-        return AttentionSchedule;
-    }
-
-    public String getOffice() {
-        return office;
-    }
-
-    public String getExperience() {
-        return experience;
-    }
-
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    public String getDocumentType() {
+        return documentType;
+    }
+
     public void setDocumentType(String documentType) {
         this.documentType = documentType;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
     }
 
     public void setDocumentNumber(String documentNumber) {
         this.documentNumber = documentNumber;
     }
 
+    public String getSpecialty() {
+        return specialty;
+    }
+
     public void setSpecialty(String specialty) {
         this.specialty = specialty;
+    }
+
+    public String getPhone() {
+        return phone;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getAttentionSchedule() {
+        return attentionSchedule;
+    }
+
     public void setAttentionSchedule(String attentionSchedule) {
-        AttentionSchedule = attentionSchedule;
+        this.attentionSchedule = attentionSchedule;
+    }
+
+    public String getOffice() {
+        return office;
     }
 
     public void setOffice(String office) {
         this.office = office;
     }
 
+    public String getExperience() {
+        return experience;
+    }
+
     public void setExperience(String experience) {
         this.experience = experience;
+    }
+
+    public ProfessionalCard getProfessionalCard() {
+        return professionalCard;
+    }
+
+    public void setProfessionalCard(ProfessionalCard professionalCard) {
+        this.professionalCard = professionalCard;
     }
 }
--- src/main/java/com/clinicaspringbootcasas/modelos/Patient.java
+++ src/main/java/com/clinicaspringbootcasas/modelos/Patient.java
@@ -8,15 +8,17 @@ public class Patient {
 @Id
 @GeneratedValue(strategy =
 GenerationType.IDENTITY)
-  private Integer id;
-  private String fullName;
+
+    private Integer id;
+    private String fullName;
     private String documentType;
     private String documentNumber;
     private String address;
     private String email;
-    private String localDateBirth;
+    private String dateBirth;
     private String gender;
     private String bloodType;
+    private String phoneNumber;
 
     @OneToOne(mappedBy = "patient")
     private ClinicalHistory clinicalHistory;
@@ -25,87 +27,105 @@ GenerationType.IDENTITY)
     public Patient() {
   }
 
-  public Patient(Integer id, String fullName, String documentType, String documentNumber, String address, String email, String localDateBirth, String gender, String bloodType) {
+  public Patient(Integer id, String fullName, String documentType, String documentNumber, String address, String email, String dateBirth, String gender, String bloodType, String phoneNumber, ClinicalHistory clinicalHistory) {
     this.id = id;
     this.fullName = fullName;
     this.documentType = documentType;
     this.documentNumber = documentNumber;
     this.address = address;
     this.email = email;
-    this.localDateBirth = localDateBirth;
+    this.dateBirth = dateBirth;
     this.gender = gender;
     this.bloodType = bloodType;
+    this.phoneNumber = phoneNumber;
+    this.clinicalHistory = clinicalHistory;
   }
 
   public Integer getId() {
     return id;
   }
 
-  public String getFullName() {
-    return fullName;
-  }
-
-  public String getDocumentType() {
-    return documentType;
-  }
-
-  public String getDocumentNumber() {
-    return documentNumber;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public String getLocalDateBirth() {
-    return localDateBirth;
-  }
-
-  public String getGender() {
-    return gender;
-  }
-
-  public String getBloodType() {
-    return bloodType;
-  }
-
   public void setId(Integer id) {
     this.id = id;
+  }
+
+  public String getFullName() {
+    return fullName;
   }
 
   public void setFullName(String fullName) {
     this.fullName = fullName;
   }
 
+  public String getDocumentType() {
+    return documentType;
+  }
+
   public void setDocumentType(String documentType) {
     this.documentType = documentType;
+  }
+
+  public String getDocumentNumber() {
+    return documentNumber;
   }
 
   public void setDocumentNumber(String documentNumber) {
     this.documentNumber = documentNumber;
   }
 
+  public String getAddress() {
+    return address;
+  }
+
   public void setAddress(String address) {
     this.address = address;
+  }
+
+  public String getEmail() {
+    return email;
   }
 
   public void setEmail(String email) {
     this.email = email;
   }
 
-  public void setLocalDateBirth(String localDateBirth) {
-    this.localDateBirth = localDateBirth;
+  public String getDateBirth() {
+    return dateBirth;
+  }
+
+  public void setDateBirth(String dateBirth) {
+    this.dateBirth = dateBirth;
+  }
+
+  public String getGender() {
+    return gender;
   }
 
   public void setGender(String gender) {
     this.gender = gender;
   }
 
+  public String getBloodType() {
+    return bloodType;
+  }
+
   public void setBloodType(String bloodType) {
     this.bloodType = bloodType;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public ClinicalHistory getClinicalHistory() {
+    return clinicalHistory;
+  }
+
+  public void setClinicalHistory(ClinicalHistory clinicalHistory) {
+    this.clinicalHistory = clinicalHistory;
   }
 }
